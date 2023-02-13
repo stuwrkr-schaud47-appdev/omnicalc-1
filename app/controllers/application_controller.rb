db49dcbdfa236bf0d471ba92c2487a26bcb74045
stuwrkr-schaud47 <stuwrkr-schaud47@mx.uillinois.edu>
2023-02-13 17:26:18 +0000

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,63 @@
 class ApplicationController < ActionController::Base
+  def blank_square_form
+    render({:template => "calculation_templates/square_form.html.erb" })
+  end
+
+  def calculate_square
+    # params = {"number"=> "42"}
+    @num = params.fetch("calculate_square").to_f
+    # @square_of_num = @num * @num
+    @square_of_num = @num ** 2
+    render({:template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def blank_random_form
+    render({:template => "calculation_templates/random_form.html.erb" })
+  end
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+    render({:template => "calculation_templates/random_results.html.erb"})
+  end  
+
+  def blank_root_form
+    render({:template => "calculation_templates/root_form.html.erb" })
+  end
+
+  def calculate_root
+    @num = params.fetch("calculate_root").to_f
+    @root_of_num = Math.sqrt(@num)
+    render({:template => "calculation_templates/root_results.html.erb"})
+  end
+
+  def blank_payment_form
+    render({:template => "calculation_templates/payment_form.html.erb"})
+  end 
+
+  def calculate_payment
+    @apr = params.fetch("apr").to_f
+    @num_of_years = params.fetch("num_years")
+    @principal = params.fetch("principal").to_f
+
+    num_of_months = @num_of_years.to_f * 12
+    # @apr = @apr.to_s(:percentage) / 100
+    # @apr = @apr.to_i / 100
+    a = @apr.to_f / 100
+    rate_per_period = a / 12
+    @apr = @apr.to_s(:percentage,{:precision =>4})
+
+
+
+    numerator = rate_per_period * @principal
+    denominator = 1 - (1 + rate_per_period)**(- num_of_months)
+    @principal = @principal.to_s(:currency, {:precision =>2})
+
+    # @payment = numerator / denominator.to_s(:currency)
+    @payment = (numerator / denominator).round(2).to_s(:currency)
+
+    render({:template => "calculation_templates/payment_results.html.erb"})
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-
+  get("/square/new", {:controller => "application", :action => "blank_square_form"})
+  get("/square/results", {:controller => "application", :action => "calculate_square"})
+  get("/random/new", {:controller => "application", :action => "blank_random_form"})
+  get("/random/results", {:controller => "application", :action => "calculate_random"})
+  get("/square_root/new", {:controller => "application", :action => "blank_root_form" })
+  get("/root/results", {:controller => "application", :action => "calculate_root"})
+  get("/payment/new", {:controller => "application", :action => "blank_payment_form"})
+  get("/payment/results", {:controller => "application", :action => "calculate_payment"})
 end
